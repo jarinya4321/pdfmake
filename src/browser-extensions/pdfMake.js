3cf6c220248fb97c43c7a3b60c3a2565a7fb1101
jarinya4321 <jarinya.nux@gmail.com>
2018-04-04 21:18:48 +0700

    Update pdfMake.js

--- src/browser-extensions/pdfMake.js
+++ src/browser-extensions/pdfMake.js
@@ -7,22 +7,22 @@ var saveAs = FileSaver.saveAs;
 
 var defaultClientFonts = {
 	Roboto: {
-		normal: 'Roboto-Regular.ttf',
-		bold: 'Roboto-Medium.ttf',
-		italics: 'Roboto-Italic.ttf',
-		bolditalics: 'Roboto-MediumItalic.ttf'
+		normal: '/Roboto-Regular.ttf',
+		bold: '/Roboto-Medium.ttf',
+		italics: '/Roboto-Italic.ttf',
+		bolditalics: '/Roboto-MediumItalic.ttf'
 	},
 	THSarabun: {
-		normal: 'THSarabun.ttf',
-		bold: 'THSarabun-Bold.ttf',
-		italics: 'THSarabun-Italic.ttf',
-		bolditalics: 'THSarabun-Bold-Italic.ttf'
+		normal: '/THSarabun.ttf',
+		bold: '/THSarabun-Bold.ttf',
+		italics: '/THSarabun-Italic.ttf',
+		bolditalics: '/THSarabun-Bold-Italic.ttf'
 	},
 	THSarabunNew: {
-		normal: 'THSarabunNew.ttf',
-		bold: 'THSarabunNew-Bold.ttf',
-		italics: 'THSarabunNew-Italic.ttf',
-		bolditalics: 'THSarabunNew-BoldItalic.ttf'
+		normal: '/THSarabunNew.ttf',
+		bold: '/THSarabunNew-Bold.ttf',
+		italics: '/THSarabunNew-Italic.ttf',
+		bolditalics: '/THSarabunNew-BoldItalic.ttf'
 	},
 };
 
